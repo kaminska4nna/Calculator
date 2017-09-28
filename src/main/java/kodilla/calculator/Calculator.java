1218d6818854d1d6c0d4ddecd391a6fd07ee413b
kaminska4nna <kaminska4nna@users.noreply.github.com>
2017-09-28 11:02:26 +0200

    calc

---
+++ src/main/java/kodilla/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package kodilla.calculator;
+
+public class Calculator {
+    public int addition(int x, int y)
+    {
+        return x + y;
+    }
+    public int subtraction(int x, int y)
+    {
+        return x - y;
+    }
+
+    public static void main (String [] args){
+        System.out.println("It works!");
+
+    }
+}
